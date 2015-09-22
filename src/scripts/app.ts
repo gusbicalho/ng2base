c82e364a3e7ae1c3a1cad97452aaedf447f8b54d
Gustavo Bicalho <gusbicalho@gmail.com>
2015-09-22 20:51:26 -0300

    Instala deps e typings, acrescenta código de exemplo

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,1 +1,23 @@
-// app.ts
+/// <reference path="../../typings/tsd.d.ts" />
+import 'zone.js';
+import 'reflect-metadata';
+
+import {Component, View, bootstrap,
+        NgFor} from 'angular2/angular2';
+
+@Component({
+  selector: 'my-app'
+})
+@View({
+  template: '<h1 *ng-for="#name of names">Oi {{name}}!</h1>',
+  directives: [NgFor]
+})
+export class MyAppComponent {
+  names: string[];
+  
+  constructor() {
+    this.names = ['Alice', 'Bob', 'Charlie'];
+  }
+}
+
+bootstrap(MyAppComponent);
