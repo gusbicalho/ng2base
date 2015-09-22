3fa0c05c5d164c1093656eaec5cedff63c4025fe
Gustavo Bicalho <gusbicalho@gmail.com>
2015-09-22 20:50:24 -0300

    Cria tarefas dev

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,68 @@
-// gulpfile.js
+var gulp = require('gulp');
+// args: pega os argumentos da linha de comando como array
+var args = require('yargs').argv;
+// config: nosso arquivo de configuração
+var config = require('./gulp.config')();
+// plugins: Carrega todos os módulos gulp-*
+var plugins = require('gulp-load-plugins')();
+// browserify e watchify
+var browserify = require('browserify');
+var watchify = require('watchify');
+// source: transforma o output do browserify em um objeto que
+// o gulp consegue manipular
+var source = require('vinyl-source-stream');
+
+gulp.task('dev', ['dev-watchify', 'dev-serve']);
+
+gulp.task('dev-serve', function() {
+  log('Iniciando servidor: [' +
+      [].concat(config.temp).concat(config.src).join(', ') +
+      ']');
+  var port = args.port || config.devServerPort;
+  plugins.connect.server({
+    root: [config.temp, config.src],
+    fallback: config.index,
+    port: port,
+    livereload: true
+  });
+  gulp.watch(config.watchReload, function() {
+    return gulp.src(config.watchReload, {read: false})
+            .pipe(plugins.connect.reload());
+  });
+});
+
+gulp.task('dev-watchify', function() {
+  log('Iniciando watchify');
+  // Cria o bundler watchify
+  var bundler = watchify(browserify(config.tsMain, watchify.args));
+  // Ativa o tsify para transpilar nossos arquivos TS
+  bundler.plugin('tsify', config.tsconfig);
+  // Sempre que houver mudança, gera novo bundle
+  bundler.on('update', watchifyBundle);
+  bundler.on('log', log); // Escreve logs no terminal
+
+  return watchifyBundle();
+
+  function watchifyBundle() {
+    return bundler.bundle()
+      .on('error',
+         plugins.util.log.bind(plugins.util, 'Browserify Error'))
+      .pipe(source(config.jsBundle))
+      .pipe(gulp.dest(config.temp))
+      .pipe(plugins.connect.reload());
+  }
+});
+
+///////////////////////////////////////
+
+function log(msg) {
+  if (typeof(msg) === 'object') {
+    for (var item in msg) {
+      if (msg.hasOwnProperty(item)) {
+        plugins.util.log(plugins.util.colors.blue(msg[item]));
+      }
+    }
+  } else {
+    plugins.util.log(plugins.util.colors.blue(msg));
+  }
+}
