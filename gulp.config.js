4b5e198a65d588b76a2da3c397e1743674fc1fcf
Gustavo Bicalho <gusbicalho@gmail.com>
2015-09-22 20:49:51 -0300

    Preenche configurações do build

--- gulp.config.js
+++ gulp.config.js
@@ -1,1 +1,32 @@
-// gulp.config.js
+module.exports = function() {
+  // Pega informações do arquivo tsconfig.
+  var tsconfig = require('./tsconfig.json');
+  var temp = '.temp/';
+  var src = 'src/';
+
+  var config = {
+    /** Diretório de arquivos compilados para dev */
+    temp: temp,
+    /** Arquivos fonte */
+    src: src,
+    /**
+     * Arquivos principais do app
+     * (processamento parte recursivamente daqui)
+     */
+    tsMain: tsconfig.files,
+    tsconfig: tsconfig,
+    /** Nome do bundle gerado pelo browserify */
+    jsBundle: 'bundle.js',
+    /** index do app */
+    index: src + 'index.html',
+    /** Porta do servidor de dev */
+    devServerPort: 5000,
+    /** Recarregar a página se algum desses arquivos mudar */
+    watchReload: [
+      src + '**/*',
+      '!' + src + '**/*.ts'
+    ],
+  };
+
+  return config;
+};
